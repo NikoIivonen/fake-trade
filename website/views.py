c2f4226cad51f1fa316b64b1bc601825b3c4b8ff
Niko Iivonen <niko.iivonen@aalto.fi>
2024-07-07 10:42:12 +0300

    improvements3

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-from .models import Note, User
+from .models import User
 from . import db
 import requests
 
