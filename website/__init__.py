a24262c2910f2c5318c38ed369fdd02111018dfa
Niko Iivonen <niko.iivonen@aalto.fi>
2024-07-10 07:32:50 +0300

    database5

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,8 @@ SYMBOLS = ['CASH', 'BTC', 'ETH', 'BNB', 'ADA', 'SOL', 'DOGE', 'AXS']
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'jdowekowaallsoweoqokp1o'
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://u6armav0gp0igh:p7b1adda8584e9ed14daabbcf67d7a35587f9547bfedb6e2a5ded292ec71cea99@cat670aihdrkt1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/db93v7f0ors0qc'
     db.init_app(app)
 
     from .views import views
